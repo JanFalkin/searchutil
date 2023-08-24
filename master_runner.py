14d30bedf15596f9fca6bd3ce5cfb82448e223d3
Jan Falkin <jan.falkin@eluv.io>
2023-08-23 17:28:47 -0700

    working happy path

--- master_runner.py
+++ master_runner.py
@@ -7,16 +7,26 @@ import string
 
 
 def run_script(input, output):
-    command = []
     try:
-        output = subprocess.Popen(["python", "process_tokens.py", input, "-1", output], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)#subprocess.check_output(command, universal_newlines=True)
+        process = subprocess.Popen(["mp/process-multi", input, "-1", output],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT,
+                                    universal_newlines=True)
+        for line in process.stdout:
+            line = line.rstrip('\n')
+            sys.stdout.write('\r' + line)
+            sys.stdout.flush()
+        process.wait()
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
 
 if __name__ == "__main__":
     base_letter = sys.argv[1]
+    base_data = sys.argv[2]
+    base_output = sys.argv[3]
+
     for letter in string.ascii_lowercase:
-        input_file = "tok-file"+base_letter+letter+".json"
-        output_file = "output"+base_letter+letter+".txt"
+        input_file = base_data+"/tok-file"+base_letter+letter+".json"
+        output_file = base_output+"/output"+base_letter+letter+".txt"
         run_script(input_file, output_file)
